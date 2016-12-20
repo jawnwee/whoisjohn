bea1c61da75a7edd0e2f9d3753297e75ee61f571
johnl <johnl@wealthfront.com>
2016-12-20 15:52:35 -0800

    collectionview

--- WhoIsJohn/JLRootViewController.swift
+++ WhoIsJohn/JLRootViewController.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class JLRootViewController: UIViewController {
   
+  let resumeCollectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    collectionView.backgroundColor = UIColor.white
+    return collectionView
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.addSubview(resumeCollectionView)
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
+  
+}
+
+extension JLRootViewController: UICollectionViewDataSource {
+  
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 2
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    return UICollectionViewCell()
+  }
+  
+}
+
+extension JLRootViewController: UICollectionViewDelegateFlowLayout {
+
+  
   
 }
