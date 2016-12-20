c7aba38b90ea181acd02320bd61f11a91ade1a0f
johnl <johnl@wealthfront.com>
2016-12-20 14:33:48 -0800

    empty app state

--- WhoIsJohn/JLRootViewController.swift
+++ WhoIsJohn/JLRootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  JLRootViewController.swift
 //  WhoIsJohn
 //
 //  Created by John Lee on 12/20/16.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class JLRootViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
+  
 }
-
